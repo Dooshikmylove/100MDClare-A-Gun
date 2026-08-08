868a52922cc7e2cace9a36830849c08c69e70850
Dooshikmylove <161066417+Dooshikmylove@users.noreply.github.com>
2026-08-08 12:56:20 -0600

    Create equipos.js

---
+++ equipos.js
@@ -0,0 +1,38 @@
+// ========================================
+// EQUIPOS
+// ========================================
+
+
+const equipos = {
+
+
+azul:{
+
+
+nombre:"Equipo Azul",
+
+color:"#2196f3",
+
+puntos:0
+
+
+},
+
+
+
+rojo:{
+
+
+nombre:"Equipo Rojo",
+
+color:"#f44336",
+
+puntos:0
+
+
+}
+
+
+};
+
+
